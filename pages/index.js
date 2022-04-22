dcc88c654b3370a98ef68284ba30f92d11eb405c
Myauti <myauti.rafael@outlook.com>
2022-04-22 00:50:43 -0400

    All my changes

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
+import styles from '../styles/styles.module.css';
+
 export default function Index() {
   return (
-    <p>
-      To test the CORS route, open the console in a new tab on a different
-      domain and make a POST / GET / OPTIONS request to <b>/api/cors</b>. Using
-      a different method from those mentioned will be blocked by CORS
-    </p>
+    <div className={styles.container}>
+      <h3>
+        Ola mundo
+      </h3>
+    </div>
   )
 }
+
+
